ccdd10543ee642a9599541c69312c0442cb82dea
Rizaldy Britanico <pluk.rizaldybritanico@gmail.com>
2024-02-11 09:40:05 +0800

    first commit

---
+++ kodepipz/src/routes/+page.svelte
@@ -0,0 +1,185 @@
+<div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="/src/hero image.jpg" alt="" width="300" height="300">
+    <h1 class="display-5 fw-bold text-body-emphasis">Welcome to our site.</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Want to learn  more about our courses and tutorials?</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+        <button type="button" class=" btn btn-primary btn-lg px-4 gap-3"><a href="./about/" style="color: white; text-decoration: none;">Visit</a></button>
+      </div>
+    </div>
+  </div>
+<br>
+<br>
+<hr class="container">
+<br>
+<br>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Web Development</h2><br>
+            <p>Web development is the process of creating and maintaining websites, encompassing everything from creating a simple single static page of plain text to complex web-based internet applications, electronic businesses, and social network services. In today's digital age, having a strong online presence is crucial for businesses and individuals alike.</p>
+            <br>
+                <p>
+                    In conclusion, web development is crucial for businesses and individuals looking to establish a strong online presence, engage with their audience, and drive business growth in the digital era. It is an essential tool for reaching a broader audience, providing valuable information, and driving conversions and sales.
+                </p>
+          
+        </div>
+        <br>
+        <div class="col-md-6">
+            <img src="/src/webimage.jpg">
+        </div>
+    </div>
+
+</div>
+  <br>
+  <br>
+  <hr class="container">
+  <div class="container">
+    <div class="row">
+        <br>
+        <div class="col-md-6">
+            <img src="/src/webimage.jpg">
+        </div>
+        <div class="col-md-6">
+           <h2>Tutorials</h2>
+           <p>At KodePipz, we offer different tutorials for kids and adults</p>
+        </div>
+    </div>
+
+</div>
+<br>
+<br>
+<hr class="container">
+  <div class="row g-4 py-5 row-cols-1 row-cols-lg-3 container" style="padding-left: 120px;">
+    <div class="feature col">
+      <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+        <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg>
+      </div>
+      <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+      <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+      <a href="#" class="icon-link">
+        Call to action
+        <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+      </a>
+    </div>
+    <div class="feature col">
+      <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+        <svg class="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg>
+      </div>
+      <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+      <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+      <a href="#" class="icon-link">
+        Call to action
+        <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+      </a>
+    </div>
+    <div class="feature col">
+      <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
+        <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg>
+      </div>
+      <h3 class="fs-2 text-body-emphasis">Featured title</h3>
+      <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+      <a href="#" class="icon-link">
+        Call to action
+        <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+      </a>
+    </div>
+  </div>
+  <br>
+  <br>
+  <hr class="container">
+  <br>
+  <br>
+  <center><h2 class="featurette-heading fw-normal lh-1">Frequently Asked Questions</h2></center><br>
+  <div class="container">
+    <div class="accordion accordion-flush card container" id="accordionFlushExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+              Accordion Item #1
+            </button>
+          </h2>
+          <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+              Accordion Item #2
+            </button>
+          </h2>
+          <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+          </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                Accordion Item #2
+              </button>
+            </h2>
+            <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                Accordion Item #2
+              </button>
+            </h2>
+            <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                Accordion Item #2
+              </button>
+            </h2>
+            <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+            </div>
+          </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+              Accordion Item #3
+            </button>
+          </h2>
+          <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+          </div>
+        </div>
+      </div>
+  </div>
+  <br>
+  <br>
+  <hr class="container">
+  <br>
+  <br>
+  <center><h2 class="featurette-heading fw-normal lh-1">Do you have a question?</h2></center>
+  <br>
+  <br>
+  <div class="container">
+    <div>  <label for="exampleFormControlInput1" class="form-label">First and Last name</label>
+        <div class="mb-3">
+            <div class="input-group">
+                <input type="text" aria-label="First name" class="form-control">
+                <input type="text" aria-label="Last name" class="form-control">
+              </div>
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Write your message</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <center><button type="button" class="btn btn-primary">Submit</button></center>
+    </div>
+  </div>
+  <br>
+  <br>
---
+++ kodepipz/src/routes/about/+page.svelte
@@ -0,0 +1,74 @@
+
+<br>
+
+<center><h2>Our  Team</h2></center>
+<br>
+<br>
+<div class="container">
+    <div class="card-group">
+        <div class="card">
+          <img src="/src/prfle.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Rizaldy Britanico</h5>
+            <p class="card-text">One of the founders of KodePipz</p>
+            <p class="card-text"><small class="text-body-secondary"></small></p>
+          </div>
+        </div>
+        <div class="card">
+            <!--Please put your photo here sir-->
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Jhenbert Villamucho</h5>
+            <p class="card-text">One of the founders of KodePipz</p>
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+</div>
+<br>
+<br>
+<hr class="container">
+<br>
+<br>
+<center><h2>The Founders</h2></center>
+<br>
+<br>
+<div class="container">
+    <div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, in consectetur! Tempore distinctio assumenda vel quaerat in nostrum. Maiores sit culpa quo ad architecto nam nostrum aut earum voluptatum a!</p>
+    </div>
+</div>
+<br>
+<br>
+<hr class="container">
+<br>
+<br>
+<center><h2>Company Vision</h2></center>
+<br>
+<br>
+<div class="container">
+    <div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, in consectetur! Tempore distinctio assumenda vel quaerat in nostrum. Maiores sit culpa quo ad architecto nam nostrum aut earum voluptatum a!</p>
+    </div>
+</div>
+<br>
+<br>
+<hr class="container">
+<br>
+<br>
+<center><h2>Company Missions</h2></center>
+<br>
+<br>
+<div class="container">
+    <div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, in consectetur! Tempore distinctio assumenda vel quaerat in nostrum. Maiores sit culpa quo ad architecto nam nostrum aut earum voluptatum a!</p>
+    </div>
+</div>
