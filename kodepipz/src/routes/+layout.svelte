89cdc0f4f901742d911ea16dc36bdd46e1e10889
Rizaldy Britanico <pluk.rizaldybritanico@gmail.com>
2024-02-12 21:11:23 +0800

    latest commit

--- kodepipz/src/routes/+layout.svelte
+++ kodepipz/src/routes/+layout.svelte
@@ -1,4 +1,3 @@
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary container-fluid">
     <div class="container-fluid">
       <a class="navbar-brand" href="#"><span style="color:blue;">Kode</span><span style="color:red;">Pipz</span></a>
--- kodepipz/src/routes/+page.svelte
+++ kodepipz/src/routes/+page.svelte
@@ -10,7 +10,8 @@
     </div>
   </div>
   -->
-  <div id="carouselExampleInterval" class="carousel slide container-fluid" data-bs-ride="carousel">
+  <!--
+    <div id="carouselExampleInterval" class="carousel slide container-fluid" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active" data-bs-interval="10000">
         <img src="/src/webimage.jpg" class="d-block w-100" alt="..." height="500px">
@@ -31,6 +32,26 @@
       <span class="visually-hidden">Next</span>
     </button>
   </div>
+  -->
+  <br>
+  <br>
+  <div class="container">
+   <div class="row">
+<div class="col-md-6">
+  <aside style="padding-left: 40px;">
+    <img src="/src/web1.jfif" alt=""><br>
+    <img src="/src/web2.jfif" alt="" style="border-radius: 50%;">
+    <img src="/src/web3.jfif" alt="">
+  </aside>
+</div>
+<div class="col-md-6"><br>
+  <h1>Welcome to our site!</h1>
+  <h5 style="font-weight: 300;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure eaque voluptate, eligendi omnis consequatur nihil similique totam. Magnam, doloribus. Ut corporis sapiente corrupti accusamus fugiat nesciunt ipsam inventore odio magni.</h5>
+  <br>
+  <button class="btn btn-primary" type="button" value="submit">Get Started</button>
+</div>
+   </div>
+  </div>
 <br>
 <br>
 <hr class="container">
@@ -40,7 +61,7 @@
     <div class="row">
         <div class="col-md-6">
             <h2>Why study web development?</h2><br>
-            <p>In today's digital age, businesses and organizations of all sizes rely heavily on an online presence. This has led to a significant demand for skilled web developers who can design, build, and maintain websites and web applications.</p><br>
+            <h5 style="font-weight: 300;">In today's digital age, businesses and organizations of all sizes rely heavily on an online presence. This has led to a significant demand for skilled web developers who can design, build, and maintain websites and web applications.</h5><br>
             <button class="btn btn-primary" type="submit">Apply now!</button>
         </div>
         <br>
@@ -63,7 +84,7 @@
         </div>
         <div class="col-md-6">
            <h2>Online Learning</h2>
-           <p>At KodePipz, we also offer online tutorial for kids and adults on different subjects. You can pick your preferred teachers or tutors. Do you want to check our available teachers/tutors as well as the subjects we're currently offering?</p>
+           <h5 style="font-weight: 300;">At KodePipz, we also offer online tutorial for kids and adults on different subjects. You can pick your preferred teachers or tutors. Do you want to check our available teachers/tutors as well as the subjects we're currently offering?</h5>
            <button class="btn btn-primary" type="submit">Signup now!</button>
         </div>
     </div>
@@ -78,7 +99,7 @@
         <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"></use></svg>
       </div>
       <h3 class="fs-2 text-body-emphasis">Affordability</h3>
-      <p>Our web development course is designed to be affordable and accessible to a wide range of individuals who are interested in learning about this dynamic field. The course provides comprehensive and practical knowledge at a reasonable price point, making it an excellent investment for those looking to acquire valuable skills in web development.</p><br>
+      <h5 style="font-weight: 300;">Our web development course is designed to be affordable and accessible to a wide range of individuals who are interested in learning about this dynamic field. The course provides comprehensive and practical knowledge at a reasonable price point, making it an excellent investment for those looking to acquire valuable skills in web development.</h5><br>
       
       <a href="#" class="icon-link">
      Apply now!
@@ -90,7 +111,7 @@
         <svg class="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg>
       </div>
       <h3 class="fs-2 text-body-emphasis">Flexibility</h3>
-      <p>Our web development course offers exceptional flexibility when it comes to scheduling. We understand that our students have diverse commitments and responsibilities, which is why we've designed the course to accommodate various schedules.</p>
+      <h5 style="font-weight: 300;">Our web development course offers exceptional flexibility when it comes to scheduling. We understand that our students have diverse commitments and responsibilities, which is why we've designed the course to accommodate various schedules.</h5>
       <a href="#" class="icon-link">
         Apply now!
         <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
@@ -101,7 +122,7 @@
         <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"></use></svg>
       </div>
       <h3 class="fs-2 text-body-emphasis">Accessibility</h3>
-      <p>Our web development course places a strong emphasis on accessibility. We believe that everyone, regardless of their abilities, should have equal access to educational opportunities. By integrating accessibility features into our course, we strive to create a digital space that accommodates diverse learning needs.</p>
+      <h5 style="font-weight: 300;">Our web development course places a strong emphasis on accessibility. We believe that everyone, regardless of their abilities, should have equal access to educational opportunities. By integrating accessibility features into our course, we strive to create a digital space that accommodates diverse learning needs.</h5>
       <a href="#" class="icon-link">
        Apply now!
         <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
@@ -113,4 +134,14 @@
   <hr class="container">
   <br>
   <br>
-  
+<div class="container">
+  <aside>
+    <div class="" style="width: 200px;">
+<h2>Title</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque commodi sunt omnis magni. Iure cum voluptatem ducimus, dolor tempore velit suscipit eligendi? Dolor dolorem debitis dicta quas deleniti. Quod, velit!</p>
+    </div>
+  </aside>
+</div>
+<center>
+  <p>This is the center</p>
+</center>
