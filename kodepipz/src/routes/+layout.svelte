cb73c803abf223d0e00e8dcb31324afa3db5d25d
Rizaldy Britanico <pluk.rizaldybritanico@gmail.com>
2024-02-14 18:52:05 +0800

    updated the footer

--- kodepipz/src/routes/+layout.svelte
+++ kodepipz/src/routes/+layout.svelte
@@ -38,7 +38,7 @@
   <slot/>
   <br>
   
-  <footer class="py-3 my-4">
+  <footer class="py-3 my-4" style="background-color: whitesmoke;">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
       <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
       <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Tutor now</a></li>
