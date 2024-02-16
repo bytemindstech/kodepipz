65d07a0b88ac9e12553aee2a8dbef99d0a693fa3
Rizaldy Britanico <pluk.rizaldybritanico@gmail.com>
2024-02-16 23:40:48 +0800

    new commit

--- kodepipz/src/lib/hero.svelte
+++ kodepipz/src/lib/hero.svelte
@@ -10,20 +10,21 @@
   <img src="/web2.jfif" alt="image">
   <img src="/web6.jfif" alt="image">
 </aside>
+<br>
+<br>
 </div>
 <div class="col-md-6 hero1"><br>
 <h1 style="color: white;">{greet}</h1><br>
 <h5 style="font-weight: 300; color: white;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure eaque voluptate, eligendi omnis consequatur nihil similique totam. Magnam, doloribus. Ut corporis sapiente corrupti accusamus fugiat nesciunt ipsam inventore odio magni.</h5>
 <br>
 <center><button class="btn btn-primary " type="button" value="submit">Get Started</button></center>
+<br>
 </div>
  </div>
 </div>
-
 <style>
   .hero {
     background-color: #185863;
-    height: 80vh;
     padding-top: 30px;
   }
 
