4a954529e2fd9f78a81b4c8b97e505011b7fca05
Jhenbert Villamucho <jvv205756@gmail.com>
2024-02-19 00:03:49 +0800

    added getAllUsers

--- kodepipz/src/lib/server/user.service.ts
+++ kodepipz/src/lib/server/user.service.ts
@@ -1,5 +1,30 @@
 import { db } from "./db";
 
+/**
+ * @function
+ * Retrieve all users from the database
+ * @returns
+ * user object
+ */
+export const getAllUsers = async () => {
+  return db.user.findMany({
+    select: {
+      id: true,
+      username: true,
+      email: true,
+      createdAt: true,
+      firstName: true,
+      lastName: true,
+      password: true,
+      profile: true,
+    },
+  });
+};
+
+/**
+ * @function
+ * Create a new user in the database
+ */
 export const createUser = async (
   user: Omit<User, "id" | "createdAt">
 ): Promise<User> => {
