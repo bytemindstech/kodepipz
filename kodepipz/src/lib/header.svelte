fd2221fffbb209cf5e3cc5e78e78d056cfb2fa90
IT-MIT (Ilonggo Takon - Murag IT <dipo.eastern.intel@gmail.com>
2024-02-22 23:04:28 +0800

    minor commit

--- kodepipz/src/lib/header.svelte
+++ kodepipz/src/lib/header.svelte
@@ -3,27 +3,31 @@
     <div class="col-md-6">
       <h3><span class="span1">Kode</span><span class="span2">Pipz</span></h3>
     </div>
-    <marquee style="color: black; font-size: 18px; font-weight: 200;"><img src="/promo.jfif" style="height: 30px; color: blue;">WE HAVE A <strong style="color: red; font-weight: bold;">PROMO</strong> FOR YOU TO START LEARNING WEB DEVELOPMENT AND OTHER COURSES WE HAVE. </marquee>
+    <marquee style="color: black; font-size: 18px; font-weight: 200;"
+      ><img src="/promo.jfif" style="height: 30px; color: blue;" />WE HAVE A
+      <strong style="color: red; font-weight: bold;">PROMO</strong> FOR YOU TO START
+      LEARNING WEB DEVELOPMENT AND OTHER COURSES WE HAVE.
+    </marquee>
+  </div>
 </div>
 
 <style>
-    .header {
-       padding-left: 100px;
-       background-color: white;
-       padding-top: 5px;
-      
-    }
-    .span1{
-        color:blue;
-        font-size: 40px;
-    }
-    .span2 {
-        color: red;
-        font-size: 40px;
-    }
-    .btn {
-        font-size: large;
-    }
+  .header {
+    padding-left: 100px;
+    background-color: white;
+    padding-top: 5px;
+  }
+  .span1 {
+    color: blue;
+    font-size: 40px;
+  }
+  .span2 {
+    color: red;
+    font-size: 40px;
+  }
+  .btn {
+    font-size: large;
+  }
   marquee {
     font-family: cursive;
   }
