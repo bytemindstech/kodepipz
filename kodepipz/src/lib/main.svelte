0b6c581fff913f95d5f8864b031b99b5a35d3e07
Jhenbert Villamucho <jvv205756@gmail.com>
2024-02-28 19:37:51 +0800

    main.svelte

--- kodepipz/src/lib/main.svelte
+++ kodepipz/src/lib/main.svelte
@@ -10,30 +10,40 @@
   <div class="container mx-auto">
     <div class="md:flex md:items-center md:justify-center p-4">
       <div class="md:w-1/2 md:pr-8">
-        <div class="flex items-center gap-2">
-          <Avatar
-            src={webdevIcon}
-            alt="webdev-icon"
-            width="w-16"
-            rounded="rounded-full"
-          />
-          <h2>Why study web development?</h2>
+        <div class="md:flex md:items-center mt-5 gap-3 space-y-3">
+          <div>
+            <div class="flex items-center gap-2">
+              <Avatar
+                src={webdevIcon}
+                alt="webdev-icon"
+                width="w-16"
+                rounded="rounded-full"
+              />
+              <h2>Why study web development?</h2>
+            </div>
+
+            <h5 class="text-xl text-justify mt-5">
+              In today's digital age, businesses and organizations of all sizes
+              rely heavily on an online presence. This has led to a significant
+              demand for skilled web developers who can design, build, and
+              maintain websites and web applications.
+            </h5>
+
+            <button class="btn variant-filled-primary mt-5" type="submit"
+              >Apply now!</button
+            >
+          </div>
         </div>
-
-        <h5 class="text-xl text-justify mt-5">
-          In today's digital age, businesses and organizations of all sizes rely
-          heavily on an online presence. This has led to a significant demand
-          for skilled web developers who can design, build, and maintain
-          websites and web applications.
-        </h5>
-
-        <button class="btn variant-filled-primary mt-5" type="submit"
-          >Apply now!</button
-        >
       </div>
+      <img src={htmlcssjs} alt="htmlcssjs-png" class="w-100" />
+    </div>
+  </div>
+</section>
 
-      <div class="md:w-1/2 md:mt-0">
-        <!-- <img src={htmlcssjs} alt="htmlcssjs-png" /> -->
+<section>
+  <div class="container mx-auto">
+    <div class="md:flex md:items-center md:justify-center p-4">
+      <div class="md:w-1/2 md:pr-8">
         <div class="md:flex md:items-center mt-5 gap-3 space-y-3">
           <div>
             <div class="flex items-center gap-2 mt-5">
@@ -45,6 +55,7 @@
               />
               <h2>Online Learning</h2>
             </div>
+
             <h5 class="text-lg text-justify mt-5">
               At KodePipz, we also offer online tutorial for kids and adults on
               different subjects. You can pick your preferred teachers or
@@ -53,13 +64,13 @@
             </h5>
             <button class="btn variant-filled-primary mt-5">Signup now!</button>
           </div>
-          <img
-            src={onlineTeacher}
-            alt="teacher-img"
-            class="flex-auto w-1/2 mt-4 mx-auto"
-          />
         </div>
       </div>
+      <img
+        src={onlineTeacher}
+        alt="teacher-img"
+        class="flex-auto w-1/2 mt-4 mx-auto"
+      />
     </div>
   </div>
 </section>
